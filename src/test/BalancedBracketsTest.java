7999a5193b309710d5f4b431f39979697f73a040
DonaldHartley <Donald.H.Hartley@gmail.com>
2019-11-21 17:48:37 -0600

    completed studio before class

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,16 +1,35 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
-
 import static org.junit.Assert.*;
 
+
 public class BalancedBracketsTest {
-
-    //TODO: add tests here
-    @Test
-    public void emptyTest() {
-        assertEquals(true, true);
-    }
-
-
+  
+  @Test
+  public void emptyTest() { assertTrue(BalancedBrackets.hasBalancedBrackets("")); }
+  @Test
+  public void noneTest() { assertTrue(BalancedBrackets.hasBalancedBrackets("LaunchCode Rocks!")); }
+  @Test
+  public void simpleBalancedTest () { assertTrue(BalancedBrackets.hasBalancedBrackets("[]")); }
+  @Test
+  public void complexBalancedTest () { assertTrue(BalancedBrackets.hasBalancedBrackets("Launch[Code]Rocks!")); }
+  @Test
+  public void simpleUnBalancedTest () { assertFalse(BalancedBrackets.hasBalancedBrackets("[")); }
+  @Test
+  public void complexUnBalancedTest () { assertFalse(BalancedBrackets.hasBalancedBrackets("Launch[Code Rocks!")); }
+  @Test
+  public void simpleOutOfOrderTest () { assertFalse(BalancedBrackets.hasBalancedBrackets("][")); }
+  @Test
+  public void complexOutOfOrderTest () { assertFalse(BalancedBrackets.hasBalancedBrackets("Launch]Code[Rocks!")); }
+  @Test
+  public void simpleTooManyOpenTest () { assertFalse(BalancedBrackets.hasBalancedBrackets("[[]]")); }
+  @Test
+  public void complexTooManyOpenTest () { assertFalse(BalancedBrackets.hasBalancedBrackets("Launch[Code[Rocks]!")); }
+  @Test
+  public void simpleTooManyClosedTest () { assertFalse(BalancedBrackets.hasBalancedBrackets("[]]")); }
+  @Test
+  public void complexTooManyClosedTest () { assertFalse(BalancedBrackets.hasBalancedBrackets("Launch[Code]Rocks]!")); }
+  
 }
