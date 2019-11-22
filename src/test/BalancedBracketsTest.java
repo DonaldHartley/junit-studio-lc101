09245d3404c59b68b46e4b8e7291eada819470a5
DonaldHartley <Donald.H.Hartley@gmail.com>
2019-11-21 19:55:25 -0600

    completed studio bonus during lecture

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,16 +1,50 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
-
 import static org.junit.Assert.*;
 
 public class BalancedBracketsTest {
-
-    //TODO: add tests here
-    @Test
-    public void emptyTest() {
-        assertEquals(true, true);
-    }
-
-
+  
+  @Test
+  public void emptyTest() {
+    assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+  }
+  
+  @Test
+  public void noneTest() { assertTrue(BalancedBrackets.hasBalancedBrackets("LaunchCode Rocks!")); }
+  
+  /**
+   * sample test notes instead of descriptive test function test names
+   * */
+  @Test
+  public void hasBalancedBrackets_OneSetOfBalancedBrackets_ExpectTrue () { assertTrue(BalancedBrackets.hasBalancedBrackets("[]")); }
+  
+  @Test
+  public void complexBalancedTest () { assertTrue(BalancedBrackets.hasBalancedBrackets("Launch[Code]Rocks!")); }
+  
+  @Test
+  public void simpleUnBalancedTest () { assertFalse(BalancedBrackets.hasBalancedBrackets("[")); }
+  
+  @Test
+  public void complexUnBalancedTest () { assertFalse(BalancedBrackets.hasBalancedBrackets("Launch[Code Rocks!")); }
+  
+  @Test
+  public void simpleOutOfOrderTest () { assertFalse(BalancedBrackets.hasBalancedBrackets("][")); }
+  
+  @Test
+  public void complexOutOfOrderTest () { assertFalse(BalancedBrackets.hasBalancedBrackets("Launch]Code[Rocks!")); }
+  
+  @Test
+  public void simpleManyOpenTest () { assertTrue(BalancedBrackets.hasBalancedBrackets("[[]]")); }
+  
+  @Test
+  public void complexManyOpenTest () { assertFalse(BalancedBrackets.hasBalancedBrackets("Launch[Code[Rocks]!")); }
+  
+  @Test
+  public void simpleTooManyClosedTest () { assertFalse(BalancedBrackets.hasBalancedBrackets("[]]")); }
+  
+  @Test
+  public void complexTooManyClosedTest () { assertFalse(BalancedBrackets.hasBalancedBrackets("Launch[Code]Rocks]!")); }
+  
 }
